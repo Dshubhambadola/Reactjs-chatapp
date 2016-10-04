00a988ea432243a39821cecc4d4af05833f7f6f2
Shubham Badola <shubham.badola@medibox.in>
2016-10-04 17:59:25 +0530

    ninth commit

---
+++ chat-component.js
@@ -0,0 +1,51 @@
+/**
+ * Created by shubham on 4/10/16.
+ */
+
+
+ var ChatApp =  window.React.createClass({
+     getInitialState : function(){
+           return {
+               messages : [],
+               socket : window.io('http://localhost:3000')
+           }
+     },
+    componentDidMount : function () {
+         var self = this;
+      this.state.socket.on("recive-message", function (msg) {
+          var messages = self.state.messages;
+            messages.push(msg)
+          self.setState({messages: messages})
+            console.log(self.state.messages);
+      });
+    },
+    submitMessage: function(){
+      var message = document.getElementById("message").value;
+        this.state.socket.emit("new-message", message);
+
+    },
+
+   render : function(){
+
+      var self = this;
+       var messages = self.state.messages.map(function (msg) {
+          return <li>{msg}</li>
+       });
+
+       return (
+           <div>
+               <ul>
+                   {messages}
+               </ul>
+               <input id="message" type="text"/><button onClick={() => self.submitMessage()}></button>
+           </div>
+       )
+   }
+
+  });
+
+
+  window.ReactDOM.render(
+       <ChatApp/>,
+       document.getElementById("chat")
+   );
